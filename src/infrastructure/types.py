a43f80cb4623881318744d9d260dcc88b48f5fdf
jhartum <sasha.brezgunov@gmail.com>
2024-08-15 13:06:53 +0700

    remove storage

--- src/infrastructure/types.py
+++ src/infrastructure/types.py
@@ -1,6 +1,10 @@
 import msgspec
 
 
-class BaseStruct(msgspec.Struct, frozen=True, kw_only=True):
+class FrozenStruct(msgspec.Struct, frozen=True, kw_only=True):
+    pass
+
+
+class BaseStruct(FrozenStruct):  # type: ignore
     def to_dict(self):
         return {f: getattr(self, f) for f in self.__struct_fields__}
